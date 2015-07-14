0b785a78ae1453bc465e4af0267e128ff57046bc
Lilia <lilialanceiro@gmail.com>
2015-07-14 18:12:49 -0300

    Aula 14/07

---
+++ Exercícios/Aula_14072015/Aula_14072015/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14072015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cachorro fido = new Cachorro();
+            fido.SetTamanhoCm(95);
+            fido.SetNome("Fido");
+            fido.SetPeso(42.3);
+
+            Console.WriteLine("Nome: {0}", fido.GetNome());
+            Console.WriteLine("Peso: {0}", fido.GetPeso());
+            Console.WriteLine("Tamanho (m): {0}", fido.GetTamanhoM());
+            Console.WriteLine("Quatidade diária de ração: {0}", fido.RacaoDiariaKg());
+
+        }
+    }
+}
