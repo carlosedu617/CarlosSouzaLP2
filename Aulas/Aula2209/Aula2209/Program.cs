ae454423f3a24d421ca84a423fecf57da32c70da
lilialanceiro <lilialanceiro@gmail.com>
2015-09-22 17:59:48 -0300

    Aula 22/09.

---
+++ Aulas/Aula2209/Aula2209/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace Aula2209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConn = "server=localhost; Database=test; Uid=root; Pwd=";
+
+            MySqlConnection conn = new MySqlConnection(strConn);
+            MySqlCommand command = new MySqlCommand();
+
+            Console.WriteLine("Registre uma nova pessoa.");
+            string nome = Console.ReadLine();
+            command.Connection = conn;
+            command.CommandText = string.Format("insert into Pessoa (Nome) values ('{0}')", nome);
+
+           
+
+            conn.Open();
+            command.ExecuteNonQuery();
+            conn.Close();
+            Console.WriteLine("Pessoa registrada.");
+        }
+    }
+}
