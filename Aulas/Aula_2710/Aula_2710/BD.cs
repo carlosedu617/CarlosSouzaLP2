eeb039d4c3b03c54fe9e6cdc2ce748d0695edede
lilialanceiro <lilialanceiro@gmail.com>
2015-10-27 17:50:05 -0300

    Aula dia 27/10

---
+++ Aulas/Aula_2710/Aula_2710/BD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Aula_2710
+{
+    class BD
+    {
+        MySqlConnection conn = new MySqlConnection("server='localhost',database=test,uid=root,pwd=");
+        MySqlCommand cmd = new MySqlCommand();
+        public MySqlDataReader SelectIsbn(string isbn)
+        {
+            cmd.Connection = conn;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            return null;
+        }
+        
+        
+    }
+}
